978796335de53a90ccba61894f70840d066c060b
JainikD <jainikdesai30@gmail.com>
2025-04-14 19:37:31 -0400

    Tracker_commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from utils import read_video, save_video
+from trackers import Tracker
+
+def main():
+    # Read Video
+    video_frames = read_video('input_videos/08fd33_4.mp4',
+                                       read_from_stub=True,
+                                       stub_path='stubs/track_stubs.pkl')
+
+    # Initialize Tracker
+    tracker = Tracker('models/best.pt')
+
+    tracks = tracker.get_object_tracks(video_frames)
+
+    # Save video
+    save_video(video_frames, 'output_videos/output_video.avi')
