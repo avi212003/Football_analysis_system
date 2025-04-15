0196773ad09f0037fecc39c91ea56a88a56e8b9a
Rahil <rahilshah0911@gmail.com>
2025-04-14 21:09:27 -0400

    created ellipse around players instead of bounding box

--- main.py
+++ main.py
@@ -3,14 +3,22 @@ from trackers import Tracker
 
 def main():
     # Read Video
-    video_frames = read_video('input_videos/08fd33_4.mp4',
-                                       read_from_stub=True,
-                                       stub_path='stubs/track_stubs.pkl')
+    video_frames = read_video('input_videos/08fd33_4.mp4')
 
     # Initialize Tracker
     tracker = Tracker('models/best.pt')
 
-    tracks = tracker.get_object_tracks(video_frames)
+    tracks = tracker.get_object_tracks(video_frames,
+                                       read_from_stub=True,
+                                      stub_path='stubs/track_stubs.pkl')
 
+    # Draw output
+    ## Draw object Tracks
+    output_video_frames = tracker.draw_annotations(video_frames, tracks)
+    
+    
     # Save video
     save_video(video_frames, 'output_videos/output_video.avi')
+    
+    if __name__ == "__main__":
+        main()
